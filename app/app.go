a065c498378f15eb7dd1b00b2fda274d32fcab53
maatko <matkovukovic25@gmail.com>
2023-12-08 19:17:31 +0100

    cleanup

--- app/app.go
+++ app/app.go
@@ -9,6 +9,8 @@ import (
 )
 
 func Initialize() {
+	///////////////////////////////
+
 	api.RegisterModel(models.User{})
 	api.RegisterModel(models.Session{})
 
@@ -18,22 +20,4 @@ func Initialize() {
 	api.RegisterRoute("/", routes.Auth{})
 
 	///////////////////////////////
-
-	user := models.User{
-		Email:    "admin@spotisong.com",
-		Username: "admin",
-		Password: "pwd123",
-	}
-
-	err := user.Save()
-	if err != nil {
-		panic(err)
-	}
-
-	session := models.Session{User: user}
-
-	err = session.Save()
-	if err != nil {
-		panic(err)
-	}
 }
