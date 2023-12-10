2eb5ca77756c58bbcecc19b361135afb396d1ffd
maatko <matkovukovic25@gmail.com>
2023-12-10 20:33:28 +0100

    added redirects to /login & /register

--- app/routes/home.go
+++ app/routes/home.go
@@ -14,6 +14,16 @@ func (home Home) Index(response http.ResponseWriter, request *http.Request) {
 	api.RenderTemplate(response, home, http.StatusOK, "base.html")
 }
 
+func (home Home) Login(response http.ResponseWriter, request *http.Request) {
+	http.Redirect(response, request, "/auth/login", http.StatusPermanentRedirect)
+}
+
+func (home Home) Register(response http.ResponseWriter, request *http.Request) {
+	http.Redirect(response, request, "/auth/register", http.StatusPermanentRedirect)
+}
+
 func (home Home) SetupRoutes(router *mux.Router) {
 	router.HandleFunc("/", home.Index)
+	router.HandleFunc("/login", home.Login)
+	router.HandleFunc("/register", home.Register)
 }
